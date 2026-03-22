46f97c5bcc8885bc91ac7666d6acc67a5690e25d
victoria-101480 <uzuegbunamvictoriaamaka@gmail.com>
2026-03-22 22:57:15 +0000

    Add environment-specific workflows

--- main.tf
+++ main.tf
@@ -49,3 +49,53 @@ resource "aws_s3_bucket_versioning" "demo" {
 }
 
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+variable "bucket_suffix" {
+  description = "Unique suffix for bucket name (use your initials)"
+  type        = string
+  default     = "vau"  # CHANGE THIS to your initials!
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "cloudburst-${vau.environment}-${var.bucket_suffix}"
+
+  tags = {
+    Name        = "CloudBurst ${vau.environment} Bucket"
+    Environment = vau.environment
+    ManagedBy   = "terraform"
+    DeployedBy  = "github-actions"
+  }
+}
+
+output "bucket_name" {
+  description = "Name of the created S3 bucket"
+  value       = aws_s3_bucket.demo.bucket
+}
+
+output "environment" {
+  description = "Deployment environment"
+  value       = var.environment
+}
+
